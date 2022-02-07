650b81b38cc52475ba9d1f6f7ed281a01eac19cf
shailendra <shailendrasethiya@gmail.com>
2022-02-07 20:10:47 +0530

    handle submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () =>  {
+  const[inputField, setInputField] = useState({firstName : '', lastName: ''});
+    
+  const handleText = (e) => {
+    e.preventDefault();
+    setInputField((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = (event) => {
+     event.preventDefault();
+     console.log(event);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-         Hello
+         Hello World
       </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="firstName" value={inputField.firstName} onChange={handleText}/>
+        <input type="text" name="lastName"  value={inputField.lastName} onChange={handleText}/>
+        <button type="submit"> Submit </button>
+      </form>
     </div>
   );
 }
