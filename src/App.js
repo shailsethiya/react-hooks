429e138f2b2ecd3f343d30ea92ec1b574eec1633
shailendra <shailendrasethiya@gmail.com>
2022-02-09 09:21:49 +0530

    useState example

--- src/App.js
+++ src/App.js
@@ -2,30 +2,22 @@ import { useState } from 'react';
 import './App.css';
 
 const App = () =>  {
-  const[inputField, setInputField] = useState({firstName : '', lastName: ''});
+  const initialState = 0;
+  const[count, setCount] = useState(initialState);
+
+  const handleIncrement = () => {
+    for(let i=0; i<5; i++){
+      setCount( prevCount => prevCount + 1)
+    }
+  }
     
-  const handleText = (e) => {
-    e.preventDefault();
-    setInputField((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value}))
-  }
-
-  const handleSubmit = (event) => {
-     event.preventDefault();
-     console.log(event);
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-         Hello World
-      </header>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="firstName" value={inputField.firstName} onChange={handleText}/>
-        <input type="text" name="lastName"  value={inputField.lastName} onChange={handleText}/>
-        <button type="submit"> Submit </button>
-      </form>
+    <div className='App'>
+    <button>{count}</button>
+    <button onClick={() => (setCount(initialState))}>Reset</button>
+    <button onClick={() => (setCount(prevCount => prevCount + 1))}>Increment</button>
+    <button onClick={() => (setCount(prevCount => prevCount - 1))}>Decrement</button>
+    <button onClick={handleIncrement}>Increment 5</button>
     </div>
   );
 }
