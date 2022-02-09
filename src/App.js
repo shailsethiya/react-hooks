60d8fe440284d4b905f805cd67a371185417a45f
shailendra <shailendrasethiya@gmail.com>
2022-02-09 16:29:04 +0530

    didmount

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import axios from 'axios';
 import './App.css';
 
 const App = () =>  {
-  const[posts, setPost] = useState([]);
+  const[post, setPost] = useState({});
+  const[id, setId] = useState(1);
+  const[idFromBtnClick, setIdFromBtnClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromBtnClick(id);
+  }
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromBtnClick}`)
     .then((res) => {
       setPost(res.data);     
     }).catch((err) => {
       console.log("err", err)
     })
-  }, [])
+  }, [idFromBtnClick])
 
 
   return (
     <div className="App">
-    {posts.map((post) => (<li key={post.id}>{post.title}</li>))}
+      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>Fetch Post</button>
+      <p>{post.title}</p>
+      {/* {posts.map((post) => (<li key={post.id}>{post.title}</li>))} */}
     </div>
   );
 }
